a6047ddbbaf495f91bb99b2fedea4406c79196bf
BrunoCamiloRibeiro <brunocamiloribeirop@gmail.com>
2026-05-03 02:49:39 -0300

    [PROJ] Configuração inicial do Program.cs

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,6 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using FabysUnha.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+// Por enquanto, usando InMemoryDatabase
+builder.Services.AddDbContext<AppDbContext>(options =>options.UseInMemoryDatabase("FabysUnhaDB"));
+
+/*
+// Para o SQL SERVER Depois
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+*/
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
