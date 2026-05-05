99afe4649becd22b790295e5e30f640c2471e484
BrunoCamiloRibeiro <brunocamiloribeirop@gmail.com>
2026-05-04 23:20:54 -0300

    [PROJ] Definição da tabela Marcas no banco e chaves compostas

--- Project/Data/AppDbContext.cs
+++ Project/Data/AppDbContext.cs
@@ -8,5 +8,18 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }    
+
+    public DbSet<Marcas> Marcas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Servicos_Agendados>()
+        .HasKey(sa => new { sa.AgendamentoNr, sa.ServicoId });
+
+        modelBuilder.Entity<Produtos_Agendados>()
+            .HasKey(pa => new { pa.AgendamentoNr, pa.ProdutoCodigo });
+    }
 }
 
