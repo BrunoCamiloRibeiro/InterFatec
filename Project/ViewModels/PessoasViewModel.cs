b39f0b0ed0e7990fa1c5f5dbb7d218566a6c1471
BrunoCamiloRibeiro <brunocamiloribeirop@gmail.com>
2026-05-05 23:51:43 -0300

    [PROJ] Definição da ViewModel abstrata de Pessoas

---
+++ Project/ViewModels/PessoasViewModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FabysUnha.ViewModels;
+
+public abstract class PessoasViewModel
+{
+    [Required(ErrorMessage = "O nome é obrigatório.")]
+    [StringLength(100, ErrorMessage = "O nome não pode exceder 100 caracteres.")]
+    public string Nome { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "O telefone é obrigatório.")]
+    [Phone(ErrorMessage = "Telefone em formato inválido.")]
+    public string Telefone { get; set; } = string.Empty;
+}
