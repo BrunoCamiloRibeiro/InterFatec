9032c7289ec2c473101efcec9f908d55bd4c57bd
BrunoCamiloRibeiro <brunocamiloribeirop@gmail.com>
2026-05-09 16:54:39 -0300

    [PROJ] feat: implementa o controller de Especialidades

--- Project/Controllers/EspecialidadesController.cs
+++ Project/Controllers/EspecialidadesController.cs
@@ -1,11 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
+using FabysUnha.Services;
+using FabysUnha.ViewModels;
+using AutoMapper;
 
 namespace FabysUnha.Controllers;
 
+
 public class EspecialidadesController : Controller
 {
-    public IActionResult Index()
+    private readonly IEspecialidadeService _especialidadeService;
+    private readonly IMapper _mapper;
+
+    public EspecialidadesController(IEspecialidadeService especialidadeService, IMapper mapper)
+    {
+        _especialidadeService = especialidadeService;
+        _mapper = mapper;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var especialidades = await _especialidadeService.ObterTodasEspecialidades();
+        var especialidadesViewModel = _mapper.Map<IEnumerable<EspecialidadeViewModel>>(especialidades);
+        return View(especialidadesViewModel);
+    }
+
+    [HttpGet]
+    public IActionResult Criar()
     {
         return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Criar(EspecialidadeViewModel especialidadeViewModel)
+    {
+        if (!ModelState.IsValid) return View(especialidadeViewModel);
+
+        var especialidade = _mapper.Map<Models.Especialidades>(especialidadeViewModel);
+        await _especialidadeService.CriarEspecialidade(especialidade);
+        return RedirectToAction(nameof(Index));
     }
 }
