1df705d18a3ab23278c25ec20cbee5b587c5499c
BrunoCamiloRibeiro <brunocamiloribeirop@gmail.com>
2026-05-09 22:53:22 -0300

    [PROJ] feat: define e implementa o service de Produtos

---
+++ Project/Services/ProdutosService.cs
@@ -0,0 +1,51 @@
+using FabysUnha.Models;
+using FabysUnha.Repositories;
+
+namespace FabysUnha.Services;
+
+public class ProdutoService : IProdutosService
+{
+    private readonly IProdutosRepository _produtoRepository;
+
+    public ProdutoService(IProdutosRepository produtoRepository)
+    {
+        _produtoRepository = produtoRepository;
+    }
+
+    public async Task<IEnumerable<Produtos>> ObterTodosProdutos()
+    {
+        return await _produtoRepository.ObterTodosProdutos();
+    }
+
+    public async Task<Produtos?> ObterProdutoPorId(int id)
+    {
+        return await _produtoRepository.ObterProdutoPorId(id);
+    }
+
+    public async Task CriarProduto(Produtos produto)
+    {
+        if(produto.Preco <= 0) throw new ArgumentException("O preço do produto deve ser maior que zero.");
+        if(produto.MarcaId <= 0) throw new ArgumentException("O produto deve estar associado a uma marca.");
+        if(string.IsNullOrWhiteSpace(produto.PathImagem)) produto.PathImagem = "/images/placeholder.jpg";
+
+        await _produtoRepository.CriarProduto(produto);
+    }
+
+    public async Task AtualizarProduto(Produtos produto)
+    {
+        if(produto.Preco <= 0) throw new ArgumentException("O preço do produto deve ser maior que zero.");
+        if(produto.MarcaId <= 0) throw new ArgumentException("O produto deve estar associado a uma marca.");
+        if(string.IsNullOrWhiteSpace(produto.PathImagem)) produto.PathImagem = "/images/placeholder.jpg";
+
+        await _produtoRepository.AtualizarProduto(produto);
+    }
+
+    public async Task ExcluirProduto(int id)
+    {
+        var produto = await _produtoRepository.ObterProdutoPorId(id);
+        if (produto != null)
+        {
+            await _produtoRepository.ExcluirProduto(produto);
+        }
+    }
+}
