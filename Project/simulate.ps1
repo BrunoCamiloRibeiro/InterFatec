0f8ce4b3f2514a0b1d91dab0f46030754c85dc10
BrunoCamiloRibeiro <brunocamiloribeirop@gmail.com>
2026-05-31 01:34:01 -0300

    [PROJ] chore: removendo arquivo inutil .nem lembro oque

--- Project/simulate.ps1
+++
@@ -1,8 +0,0 @@
-$body = "Nome=Teste&Telefone=11999999999&Data=2026-10-10&Servicos[0].ServicoId=1&Servicos[0].FuncionarioId=1&Servicos[0].Horario=14:00:00"
-
-Invoke-RestMethod -Uri "http://localhost:5188/Agendamentos/Agendar" -Method Post -ContentType "application/x-www-form-urlencoded" -Body $body -SessionVariable session
-
-# Login creates session
-Invoke-RestMethod -Uri "http://localhost:5188/Login/Login" -Method Post -ContentType "application/x-www-form-urlencoded" -Body "Telefone=11999999999&Senha=123" -WebSession $session
-# Now post again
-Invoke-RestMethod -Uri "http://localhost:5188/Agendamentos/Agendar" -Method Post -ContentType "application/x-www-form-urlencoded" -Body $body -WebSession $session
