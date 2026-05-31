4374091c8d5233222c6f0911bd017719e6f52595
BrunoCamiloRibeiro <brunocamiloribeirop@gmail.com>
2026-05-31 01:29:32 -0300

    [PROJ] refactor: ajusta modelo de funcionarios

--- Project/Models/Funcionarios.cs
+++ Project/Models/Funcionarios.cs
@@ -5,7 +5,7 @@ public class Funcionarios : Pessoas
     public decimal Salario { get; set; }
 
     // FK
-    public int EspecialidadeId { get; set; }
+    public int? EspecialidadeId { get; set; }
     public Especialidades? Especialidade { get; set; }
 
     public ICollection<Servicos_Agendados> Servicos_Agendados { get; set; } = new List<Servicos_Agendados>();
