1ec4cbebbac3ae6e72771888ddc98829c18bb2bb
BrunoCamiloRibeiro <brunocamiloribeirop@gmail.com>
2026-05-31 01:29:39 -0300

    [PROJ] refactor: atualiza contrato de produtos

--- Project/Services/Interfaces/IProdutosService.cs
+++ Project/Services/Interfaces/IProdutosService.cs
@@ -1,12 +1,14 @@
 using FabysUnha.Models;
 
+using Microsoft.AspNetCore.Http;
+
 namespace FabysUnha.Services;
 
 public interface IProdutosService
 {
     Task<IEnumerable<Produtos>> ObterTodosProdutos();
     Task<Produtos?> ObterProdutoPorId(int id);
-    Task CriarProduto(Produtos produto);
-    Task AtualizarProduto(Produtos produto);
+    Task CriarProduto(Produtos produto, IFormFile? imagemUpload);
+    Task AtualizarProduto(Produtos produto, IFormFile? imagemUpload, bool hasStatusUpdate);
     Task ExcluirProduto(int id);
 }
