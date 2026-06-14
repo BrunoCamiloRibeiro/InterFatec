f416db93172f36d5288ee37bdbffbf0f02d20833
BrunoCamiloRibeiro <brunocamiloribeirop@gmail.com>
2026-06-13 22:59:03 -0300

    [PROJ] feat: ajusta viewmodel de cadastro de cliente

--- Project/ViewModels/Clientes/ClienteRegistroViewModel.cs
+++ Project/ViewModels/Clientes/ClienteRegistroViewModel.cs
@@ -4,4 +4,8 @@ namespace FabysUnha.ViewModels;
 
 public class ClienteRegistroViewModel : PessoasViewModel
 {
+    [Required(ErrorMessage = "A senha é obrigatória.")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Senha")]
+    public string Senha { get; set; } = string.Empty;
 }
