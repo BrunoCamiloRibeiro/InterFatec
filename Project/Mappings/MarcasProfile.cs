aeaa8df5a1831975c32f1c2d1351554e688ba6ae
BrunoCamiloRibeiro <brunocamiloribeirop@gmail.com>
2026-06-14 00:01:24 -0300

    [PROJ] feat: ajusta profile de marcas

--- Project/Mappings/MarcasProfile.cs
+++ Project/Mappings/MarcasProfile.cs
@@ -4,10 +4,18 @@ using FabysUnha.ViewModels;
 
 namespace FabysUnha.Mappings;
 
+/// <summary>
+/// Perfil de mapeamento do AutoMapper para a entidade Marcas.
+/// </summary>
 public class MarcasProfile : Profile
 {
+    /// <summary>
+    /// Construtor contendo as regras de mapeamento utilizadas para marcas de produtos.
+    /// </summary>
     public MarcasProfile()
     {
+        // Estabelece mapeamento bidirecional (ida e volta) entre a entidade de dados 'Marcas' e o 'MarcasViewModel'.
+        // Isso facilita salvar novos registros e exibi-los na interface gráfica.
         CreateMap<Marcas, MarcasViewModel>().ReverseMap();
     }
 }
